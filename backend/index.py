675622aa35389b4be6e6163c281df9bded3f7f4e
ImanWiguna <rcooker00@gmail.com>
2024-03-31 20:44:38 +0700

    Test API

--- backend/index.py
+++ backend/index.py
@@ -1,1 +1,20 @@
 import streamlit as st
+import requests
+
+def getall():
+    url = "https://webapi.bps.go.id/v1/api/domain/type/all/key/04672fed2c8645d7564d60891ddc98c4/"  
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  
+
+        data = response.json()  # Mengurai respons JSON
+        return data
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data:", e)
+        return None
+
+# Contoh penggunaan
+getallData = getall()
+if getallData:
+    print(getallData)
